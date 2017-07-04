5e1069390d44bb9224eb207737108fdac0b6d507
Valenzuela <pv250078@ncr.com>
2017-07-04 00:38:45 -0300

    fix clase asistencia

--- gestorAsistencia/clases/Asistencia.php
+++ gestorAsistencia/clases/Asistencia.php
@@ -4,7 +4,7 @@
 
 //	Atributos
 	public $id;
-	public $idCurso;
+	public $idCursadaAlumno;
  	public $fecha_asistencia;
   	public $asistencia;
   	public $idAlumno;
@@ -12,7 +12,7 @@
 //	Configurar parámetros para las consultas
 	public function setQueryParams($consulta,$alumno){
 			$consulta->bindValue(':id',$alumno->id, \PDO::PARAM_INT);
-			$consulta->bindValue(':idCurso',$alumno->idCurso, \PDO::PARAM_STR);
+			$consulta->bindValue(':idCursadaAlumno',$alumno->idCursadaAlumno, \PDO::PARAM_STR);
 			$consulta->bindValue(':fecha_asistencia',$alumno->fecha_asistencia, \PDO::PARAM_STR);
 			$consulta->bindValue(':asistencia', $alumno->asistencia, \PDO::PARAM_STR);
 			$consulta->bindValue(':idAlumno', $alumno->idAlumno, \PDO::PARAM_STR);
